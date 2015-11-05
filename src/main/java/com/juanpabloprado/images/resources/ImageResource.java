926cce713efdceebc48a52ee5a22c4dcb1965819
Juan Pablo Prado <juan@juanpabloprado.com>
2015-11-05 10:41:11 -0500

    Encode file name to UTF-8

--- src/main/java/com/juanpabloprado/images/resources/ImageResource.java
+++ src/main/java/com/juanpabloprado/images/resources/ImageResource.java
@@ -5,8 +5,11 @@ import com.juanpabloprado.images.utils.FileIO;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import javax.activation.MimetypesFileTypeMap;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -15,7 +18,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
@@ -37,25 +39,30 @@ public class ImageResource {
   @Consumes(MediaType.MULTIPART_FORM_DATA)
   public Response createImage(
       @FormDataParam("file") InputStream uploadedInputStream,
-      @FormDataParam("file") FormDataContentDisposition fileDetail) throws IOException, URISyntaxException {
+      @FormDataParam("file") FormDataContentDisposition fileDetail) throws URISyntaxException, UnsupportedEncodingException {
     String uploadedFileLocation = dest + fileDetail.getFileName();
-    // save it
-    FileIO.writeToFile(uploadedInputStream, uploadedFileLocation);
-    LOGGER.info("Image uploaded to : " + uploadedFileLocation);
+    try {
+      // save it
+      FileIO.writeToFile(uploadedInputStream, uploadedFileLocation);
+      LOGGER.info("Image uploaded to : " + uploadedFileLocation);
+    } catch (IOException e) {
+      LOGGER.error(e.getMessage());
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+    }
 
-    return Response.created(new URI(fileDetail.getFileName())).build();
+    String image = URLEncoder.encode(fileDetail.getFileName(), "UTF-8");
+    return Response.created(new URI(image)).build();
   }
 
   @GET
   @Path("/{image}")
   @Produces("image/*")
-  public Response getImage(@PathParam("image") String image) {
+  public Response getImage(@PathParam("image") String image) throws UnsupportedEncodingException {
+    image = URLDecoder.decode(image, "UTF-8");
     File file = new File(dest + image);
     if (!file.exists()) {
-      throw new WebApplicationException(
-          Response.status(Response.Status.NOT_FOUND)
-              .entity(new Error("No image found"))
-              .build());
+      LOGGER.warn(image + " could not be found");
+      return Response.status(Response.Status.NOT_FOUND).entity(new Error("No image found")).build();
     }
     String mimeType = new MimetypesFileTypeMap().getContentType(file);
     return Response.ok(file, mimeType).build();
@@ -63,26 +70,26 @@ public class ImageResource {
 
   @DELETE
   @Path("/{image}")
-  public Response deleteImage(@PathParam("image") String image) {
+  public Response deleteImage(@PathParam("image") String image) throws UnsupportedEncodingException {
+    image = URLDecoder.decode(image, "UTF-8");
     try {
       File file = new File(dest + image);
       if (!file.exists()) {
-        throw new WebApplicationException(
-            Response.status(Response.Status.NOT_FOUND)
-                .entity(new Error("No image found"))
-                .build());
+        LOGGER.warn(image + " could not be found");
+        return Response.status(Response.Status.NOT_FOUND).entity(new Error("No image found")).build();
       }
 
       if (file.delete()) {
         LOGGER.info("** Deleted " + image + " **");
         return Response.noContent().build();
       } else {
-        LOGGER.info("Failed to delete " + image);
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new Error("Failed to delete " + image)).build();
+        LOGGER.warn("Failed to delete " + image);
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
       }
     } catch (SecurityException e) {
-      LOGGER.info("Unable to delete " + image);
-      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new Error("Unable to delete " + image)).build();
+      LOGGER.error("Unable to delete " + image);
+      LOGGER.error(e.getMessage());
+      return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
     }
   }
 
